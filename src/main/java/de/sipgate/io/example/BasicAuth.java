022860881e39e26fc3dd30406f5c19efe80b98a7
disterhoeft <disterhoeft@sipgate.de>
2019-04-09 15:55:38 +0200

    refactor: exception handling in main function

--- src/main/java/de/sipgate/io/example/BasicAuth.java
+++ src/main/java/de/sipgate/io/example/BasicAuth.java
@@ -8,22 +8,23 @@ import java.util.Base64;
 
 
 public class BasicAuth {
-
 	private static final String baseUrl = "https://api.sipgate.com/v2";
 
-	public static void main(String[] args) throws UnirestException {
+	public static void main(String[] args) {
 		String username = "YOUR_SIPGATE_EMAIL";
 		String password = "YOUR_SIPGATE_PASSWORD";
 
 		String credentials = username + ":" + password;
-
 		String base64EncodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
 
-		HttpResponse<String> response = Unirest.get(baseUrl + "/account")
-				.header("Authorization", "Basic " + base64EncodedCredentials)
-				.asString();
-
-		System.out.println("Status: " + response.getStatus());
-		System.out.println("Body: " + response.getBody());
+		try {
+			HttpResponse<String> response = Unirest.get(baseUrl + "/account")
+					.header("Authorization", "Basic " + base64EncodedCredentials)
+					.asString();
+			System.out.println("Status: " + response.getStatus());
+			System.out.println("Body: " + response.getBody());
+		} catch (UnirestException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
