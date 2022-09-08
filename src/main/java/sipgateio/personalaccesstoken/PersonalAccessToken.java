8b5f5fb5368e1fbc14213967db416e535d5af09e
Daniel Knerr <knerr@sipgate.de>
2022-09-08 10:31:13 +0200

    :recycle: extract variables to .env
    Co-authored-by: Mathias Bethge <bethge@sipgate.de>
    Co-authored-by: Nils Alexander Baumann <baumann@sipgate.de>

--- src/main/java/sipgateio/personalaccesstoken/PersonalAccessToken.java
+++ src/main/java/sipgateio/personalaccesstoken/PersonalAccessToken.java
@@ -3,6 +3,7 @@ package sipgateio.personalaccesstoken;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.Base64;
 
@@ -11,8 +12,10 @@ public class PersonalAccessToken {
 	private static final String baseUrl = "https://api.sipgate.com/v2";
 
 	public static void main(String[] args) {
-		String tokenId = "SIPGATE_TOKEN_ID";
-		String token = "SIPGATE_TOKEN";
+		Dotenv dotenv = Dotenv.load();
+
+		String tokenId = dotenv.get("TOKEN_ID");
+		String token = dotenv.get("TOKEN");
 
 		String credentials = tokenId + ":" + token;
 		String base64EncodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
